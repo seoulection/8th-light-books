7354afab9390af40c4c21e3b3e9df35df6df98d9
junehoe <br1ansun9@gmail.com>
2019-06-24 21:52:13 -0700

    creates and tests the search bar component for the app

--- src/__tests__/SearchBar.test.js
+++ src/__tests__/SearchBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import SearchBar from '../components/SearchBar.js';
 import '../setupTests.js';
 
@@ -7,11 +7,51 @@ describe('SearchBar', () => {
   let wrapper;
   beforeEach(() => wrapper = shallow(<SearchBar />));
 
+  it('should render', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1);
   });
 
   it('should render a form', () => {
-    expect(wrapper.containsMatchingElement(<form />)).toEqual(true);
+    expect(wrapper.find('form').length).toEqual(1);
+  });
+
+  it('should render a Book Search label', () => {
+    expect(wrapper.find('label').text()).toBe('Book Search');
+  });
+
+  it('should render an input element', () => {
+    expect(wrapper.find('input').length).toEqual(1);
+  });
+
+  it('should capture the term correctly', () => {
+    const component = mount(<SearchBar />);
+    const input = component.find('input');
+    input.instance().value = 'trees';
+    input.simulate('change');
+    expect(component.state().term).toEqual('trees');
+  });
+
+  it('should cancel the event when form is submitted', () => {
+    const onFormSubmit = jest.fn();
+    const component = mount(<SearchBar onFormSubmit={onFormSubmit} />);
+    let prevented = false;
+    component.find('form').simulate('submit', {
+      preventDefault: () => {
+        prevented = true;
+      }
+    });
+    expect(prevented).toBe(true);
+  });
+
+  it('should pass the correct props on form submit', () => {
+    const onFormSubmit = jest.fn();
+    const component = mount(<SearchBar onFormSubmit={onFormSubmit} />);
+    const form = component.find('form');
+    form.simulate('submit');
+    expect(onFormSubmit.mock.calls.length).toBe(1);
   });
 });
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  }
+
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.onFormSubmit}>
+          <label>Book Search</label>
+          <input type="text" onChange={this.onInputChange} />
         </form>
       </div>
     );
