1388ec0443dd8cd2335839de9f2879e6893742ed
junehoe <br1ansun9@gmail.com>
2019-06-25 19:14:44 -0700

    creates BookList and Book components

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Book = () => {
-  return <div>Book</div>;
+const Book = ({ book }) => {
+  const title = book.volumeInfo.title;
+  const authors = book.volumeInfo.authors;
+  const publisher = book.volumeInfo.publisher;
+  const image = book.volumeInfo.imageLinks.smallThumbnail;
+
+  return (
+    <div>
+      <div>
+        <img src={image ? image : 'No image'} alt="hello" />
+      </div>
+      <div>
+        <h1>{title ? title : 'Untitled'}</h1>
+        <h4>{authors ? authors.join(', ') : 'No author(s)'}</h4>
+        <h5>{publisher ? publisher : 'No publisher(s)'}</h5>
+      </div>
+    </div>
+  );
 }
 
 export default Book;
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Book from './Book';
 
-const BookList = () => {
-  return <div>BookList</div>;
+const BookList = ({ books }) => {
+  let renderedList;
+  if (books) {
+    renderedList = books.map((book) => {
+      return <Book key={book.id} book={book} />;
+    });
+  } else {
+    renderedList = 'No books were found'
+  }
+
+  return (
+    <div>
+      {renderedList}
+    </div>
+  );
 }
 
 export default BookList;
