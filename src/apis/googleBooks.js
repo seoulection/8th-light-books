aa583d10cfd799fbd0af3b8fec02e17fad58bddb
junehoe <br1ansun9@gmail.com>
2019-06-25 20:33:52 -0700

    calls API key from hidden .env file

--- src/apis/googleBooks.js
+++ src/apis/googleBooks.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyCuK81MFUkBvZP6bnMnPF3WmqA5S0iUsA0';
-
 export default axios.create({
   baseURL: 'https://www.googleapis.com/books/v1',
   params: {
-    key: API_KEY
+    key: process.env.API_KEY
   }
 });
