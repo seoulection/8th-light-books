513dee6dc7fe992873e17cc8fdf77b6296c2bde9
junehoe <br1ansun9@gmail.com>
2019-06-25 23:12:36 -0700

    resolves no image pathing issue

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,19 @@
 import React from 'react';
+import noImage from '../images/no-image.jpg';
 
 const Book = ({ book }) => {
   const imageStyle = {
     width: '150px',
     height: '200px'
   }
-  const noImagePath = '../public/no-image.jpg';
 
   return (
     <div className="card">
       <div className="media d-flex justify-content-center">
         <img
           className="mr-3"
-          src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : noImagePath}
-          alt="hello"
+          src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail : noImage}
+          alt="not-found"
           style={imageStyle}
         />
         <div className="media-body">
