fd47c3190544dff5298ce97e9b72eda9e7313102
junehoe <br1ansun9@gmail.com>
2019-06-25 19:13:12 -0700

    adds API call functionality in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import SearchBar from './SearchBar.js';
 import BookList from './BookList.js';
+import googleBooks from '../apis/googleBooks';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-      <BookList />
-    </div>
-  );
+class App extends React.Component {
+  state = { books: [] };
+
+  onTermSubmit = async term => {
+    const response = await googleBooks.get('/volumes', {
+      params: {
+        q: term,
+        maxResults: 40
+      }
+    });
+    this.setState({ books: response.data.items });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <BookList books={this.state.books} />
+      </div>
+    );
+  }
 }
 
 export default App;
