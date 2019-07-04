036c9905c74593d710b702fe7a1fb7f94863e72f
junehoe <br1ansun9@gmail.com>
2019-07-04 11:56:27 -0700

    reduces magic strings and destructures props for readability

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import noImage from '../images/no-image.jpg';
 
-const imageWidth = '150px';
-const imageHeight = '200px';
-const noBookTitle = 'Untitled';
-const noAuthors = 'No Author(s)';
-const noPublisher = 'No Publisher';
-const secureHTTP = 'https://';
-const unsecureHTTP = 'http://';
+const IMAGE_WIDTH = '150px';
+const IMAGE_HEIGHT = '200px';
+const IMAGE_ALT = 'not-found';
+const NO_TITLE = 'Untitled';
+const NO_AUTHORS = 'No Author(s)';
+const NO_PUBLISHER = 'No Publisher';
 
-const Book = ({ book }) => {
+const Book = ({ book: { volumeInfo: { imageLinks, title, authors, publisher, infoLink } } }) => {
 
   const imageStyle = {
-    width: imageWidth,
-    height: imageHeight
+    width: IMAGE_WIDTH,
+    height: IMAGE_HEIGHT
   }
 
   return (
@@ -21,15 +20,15 @@ const Book = ({ book }) => {
       <div className="media d-flex justify-content-center">
         <img
           className="mr-3"
-          src={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.smallThumbnail.replace(unsecureHTTP, secureHTTP) : noImage}
-          alt="not-found"
+          src={imageLinks ? imageLinks.smallThumbnail.replace('http://', 'https://') : noImage}
+          alt={IMAGE_ALT}
           style={imageStyle}
         />
         <div className="media-body">
-          <h1>Title: {book.volumeInfo.title ? book.volumeInfo.title : noBookTitle}</h1>
-          <h4>Author(s): {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : noAuthors}</h4>
-          <h5>Publisher: {book.volumeInfo.publisher ? book.volumeInfo.publisher : noPublisher}</h5>
-          <a target="_blank" rel="noopener noreferrer" href={book.volumeInfo.infoLink.replace(unsecureHTTP, secureHTTP)}>More info</a>
+          <h1>Title: {title ? title : NO_TITLE}</h1>
+          <h4>Author(s): {authors ? authors.join(', ') : NO_AUTHORS}</h4>
+          <h5>Publisher: {publisher ? publisher : NO_PUBLISHER}</h5>
+          <a target="_blank" rel="noopener noreferrer" href={infoLink.replace('http://', 'https://')}>More info</a>
         </div>
       </div>
     </div>
